28923e5cfe15457e5d260fe5f711b1d1f6beb767
Sining Li <lisining@gmail.com>
2019-07-14 00:21:32 +0800

    MOD: first workable version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="home">
-    <button v-if="state === BEFORE_START" @click="onStart">开始!</button>
+    <button v-if="state === BEFORE_START" @click="onStart">开始</button>
     <div v-if="isRunning">
       <span v-for="(sec,secIdx) in sections">
-        <span v-if="playingSecNo != secIdx">
-          <span v-for="finger in sec">
-            {{finger}}
+        <span v-if="playingSecNo == secIdx">
+          <span v-for="note in sec">
+            <span v-if="note.status === 'SUCCESS'">{{note.finger}}</span>
+            <span v-if="note.status === 'NOT_HIT'">.</span>
+          </span>
+        </span>
+        <span v-else-if="secIdx > playingSecNo && secIdx <= playingSecNo + coveredSecNum">
+          <span v-for="note in sec">
+            x
           </span>
         </span>
         <span v-else>
-          <span v-for="finger in sec">
-            .
+          <span v-for="note in sec">
+            {{note.finger}}
           </span>
         </span>
         |
       </span>
     </div>
+    <div v-if="state === RUNNING || state === ENDING ">
+      正确：{{correctNum}}
+    </div>
     <div v-if="state === ENDING">
-      <h2>游戏结束！</h2>
-      <button @click="onConfirmEnd">OK!</button>
+      <h2>游戏结束！得分{{score}}</h2>
+      <button @click="onConfirmEnd">好的</button>
     </div>
   </div>
 </template>
@@ -34,24 +43,43 @@ export default {
   },
   timers: {
     running: {
-      time: 1000,
+      time: 3000,
       autostart: false,
       repeat: true
     }
   },
   data: function() {
     return {
-      sections: [],
-      playingSecNo: -1,
-      state: st.BEFORE_START
+      sections: [],      // “乐谱”
+      playingSecNo: -1,  // 当前在演奏第几个小节
+      state: st.BEFORE_START, // 当前状态
+      coveredSecNum: 0,  // 要求提前看几个小节？
+      posInPlayingSec: 0, // 当前在演奏第几个音符
+      correctNum: 0       // 本局演奏正确的音符数
+    }
+  },
+  created: function(){
+      var _this = this;
+      window.onkeydown = (e) => {
+        this.onPress(e.key)
     }
   },
   mounted () {
 
   },
   computed: {
+    score() {
+      let s = 0
+      for(let i=0; i<this.sections.length; i++) {
+        s += this.sections[i].length
+      }
+      return Math.floor(this.correctNum / s * 100)
+    },
     isRunning () {
       return this.state === st.RUNNING
+    },
+    RUNNING () {
+      return st.RUNNING
     },
     BEFORE_START () {
       return st.BEFORE_START
@@ -62,14 +90,33 @@ export default {
   },
   methods: {
     onStart() {
-      this.state = st.RUNNING
       let sectionNum = 10
       let notesInSection = 4
       let fingerCount = 4
       this.createSections({sectionNum, notesInSection, fingerCount})
-      this.playingSecNo = -1
+      this.playingSecNo = -1 - this.coveredSecNum
+      this.correctNum = 0
+      this.posInPlayingSec = 0
+      this.state = st.RUNNING
       // https://www.npmjs.com/package/vue-timers
       this.$timer.start('running')
+    },
+    onPress(key) {
+      console.log(`key=${key}`)
+      if( this.state == st.RUNNING) {
+        if(this.playingSecNo >= 0) {
+          let playingSec = this.sections[this.playingSecNo]
+          if(this.posInPlayingSec < playingSec.length) {
+            let expectNote = playingSec[this.posInPlayingSec]
+            console.log(`expectNote=${expectNote}`)
+            if(expectNote.finger == key ) {
+              this.correctNum ++
+              this.posInPlayingSec ++
+              expectNote.status = 'SUCCESS'
+            }
+          }
+        }
+      }
     },
     running () {
       console.log('Hello world' + new Date())
@@ -78,6 +125,7 @@ export default {
         this.state = st.ENDING
       } else {
         this.playingSecNo++
+        this.posInPlayingSec = 0
       }
     },
     createSections({sectionNum, notesInSection, fingerCount}) {
@@ -85,8 +133,9 @@ export default {
       for(let i=0; i<sectionNum; i++) {
         let section = []
         for(let j=0; j<notesInSection; j++) {
-          let note = Math.floor(Math.random() * fingerCount) + 1
-          section.push(note)
+          let finger = Math.floor(Math.random() * fingerCount) + 1
+          let status = 'NOT_HIT'
+          section.push({finger, status})
         }
         this.sections.push(section)
       }
